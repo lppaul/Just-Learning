f7c7412c8c87b3b3c15637e018c7a7410f46d9ce
lppaul <37919701+lppaul@users.noreply.github.com>
2018-03-30 16:51:10 -0400

    Add files via upload

---
+++ games.py
@@ -0,0 +1,17 @@
+class Baseball:
+
+    def atBat(self):
+        outs = 0
+        runs = 0
+
+        print('Press "1" to swing')
+        swing = int(input())
+        if swing == 1:
+            print('hit')
+
+
+    def startGame(self):
+        while True:
+            print('Welcome to the game, simply press a key to continue through the plays')
+            Baseball.atBat('')
+
